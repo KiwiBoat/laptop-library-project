e5ce800080ac54658f71ded134b55b9ca1aaae9f
school <school@192.168.0.150>
2022-12-11 20:39:21 +0100

    yesterday commit test 2

---
+++ laptopLibraryProject/src/main/java/be/ehb/laptopLibraryProject/laptopLibraryProject/LaptopLibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.laptopLibraryProject.laptopLibraryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopLibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopLibraryProjectApplication.class, args);
+	}
+
+}
---
+++ laptopLibraryProject/src/test/java/be/ehb/laptopLibraryProject/laptopLibraryProject/LaptopLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.laptopLibraryProject.laptopLibraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopLibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
